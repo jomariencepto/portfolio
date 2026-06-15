18e367a2a9475b876a177aff23aad1f378d11609
Jomari Encepto <enceptojomari.pdm@gmail.com>
2026-06-15 20:33:07 +0800

    Update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaIceCream, FaReceipt, FaRobot } from 'react-icons/fa';
+import {  FaIceCream, FaReceipt, FaRobot } from 'react-icons/fa';
 
 export default function Projects({ darkMode }) {
   const projects = [
